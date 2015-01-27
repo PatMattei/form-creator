707ce8a91075bb2c2d13c53444cb67844cb51325
Patrick Mattei <patrick.mattei@gmail.com>
2015-01-27 17:12:10 -0500

    Extracted dropdown options

--- json_creator.rb
+++ json_creator.rb
@@ -1,17 +1,19 @@
 require 'rubyXL'
 
-workbook = RubyXL::Parser.parse("posting-docs.xlsx")
+def open_workbook(sheet)
+  workbook = RubyXL::Parser.parse("posting-docs.xlsx")
+  fields = workbook[sheet]
+end
 
 def create_file
   File.delete('fields.json')
   File.new('fields.json', 'w')
 end
 
-def create_FormFields(workbook)
+def create_FormFields
   form_fields = []
 
-
-  fields = workbook['Fields']
+  fields = open_workbook('Fields')
   fields_sheet = fields.extract_data
 
   field_titles = fields_sheet[0]
@@ -19,13 +21,22 @@ def create_FormFields(workbook)
 
 
   field_values.map do |value|
-    form_fields << Hash[field_titles.zip(value)]
+    new_field = Hash[field_titles.zip(value)]
+    
+    form_fields << new_field
+
+    if new_field['InputType'] == "Dropdown"
+      input_name = new_field['InputName']
+      create_DropDownOptions(input_name)
+    end
   end
-  puts "#{form_fields}"
 end
 
 
-
+def create_DropDownOptions(input_name)
+  dropdown_sheet = open_workbook("#{input_name}Options")
+  dropdown_data = dropdown_sheet.extract_data
+end
 
 create_file
-create_FormFields(workbook)
+create_FormFields
